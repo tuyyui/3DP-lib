855eda7f3e97de9e427b04fe584b31417b8fb52f
tuyyui <mrjustinyoung991@gmail.com>
2020-08-14 20:15:10 -0500

    Updated raycast.h file with new formulas and operators
    New formulas and operators has been added to the raycast. Also comment added to the top of file.

--- src/include/raycastengine.h
+++ src/include/raycastengine.h
@@ -1,10 +1,10 @@
-
 #ifndef RAYCASTENGINE_H
 #define RAYCASTENGINE_H
 #include <iostream>
 #include <cmath>
 #include <vector>
-//RayCaster engine
+//RAYTRACER DURR engine
+/// LOOK I KNOW IT'S A RAYTRACER BUT I'M A DUMMY AND KEEP GETTING THE NAMES MIXED UP DON'T GET MAD AT ME.
 namespace raycast
 {
     template <typename T>
@@ -23,30 +23,68 @@ namespace raycast
         T get_z() const { return z_pos; }
 
         /*
+
 -- Operator Overloading
+
     */
-        //Scalar
-        Vector<T> operator*(const T &d) const { return Vector<T>(x_pos * d, y_pos * d, z_pos * d); };
-        //Multiplying Vectors
-        Vector<T> operator*(const Vector<T> &v) const { return Vector<T>(x_pos * v.x_pos, y_pos * v.y_pos, z_pos * v.z_pos); };
+
+        Vector<T> operator*(const T &d) const { return Vector<T>(x_pos * d, y_pos * d, z_pos * d); }
+
+        Vector<T> operator*(const Vector<T> &v) const { return Vector<T>(x_pos * v.x_pos, y_pos * v.y_pos, z_pos * v.z_pos); }
         Vector<T> operator/(const T &d) const { return Vector<T>(x_pos / d, y_pos / d, z_pos / d); }
         Vector<T> operator+(const Vector<T> &v) const { return Vector<T>(x_pos + v.x_pos, y_pos + v.y_pos, z_pos + v.z_pos); }
         Vector<T> operator-(const Vector<T> &v) const { return Vector<T>(x_pos - v.x_pos, y_pos - v.y_pos, z_pos - v.z_pos); }
-        //Formula
-        T dot_product(Vector<T> a);
-        Vector<T> cross_product(Vector<T> a);
+        friend inline std::ostream &operator<<(std::ostream &os, Vector<T> a)
+        {
+            os << a.x_pos << " " << a.y_pos << " " << a.z_pos << std::endl;
+            return os;
+        }
+        friend inline std::istream &operator>>(std::istream &is, Vector<T> a)
+        {
+            is >> a.x_pos >> a.y_pos >> a.z_pos;
+            return is;
+        }
+        Vector<T> operator+=(const Vector<T> &v)
+        {
+            *this = v.normalize();
+            return (*this);
+        }
+        /*
+----------- FORMULAS ----------------------------
+
+        */
+        T dot_product(Vector<T> a)
+        {
+            return x_pos * a.x_pos + y_pos * a.y_pos + z_pos * a.z_pos;
+        }
+        Vector<T> cross_product(Vector<T> a) { return Vector<T>((get_y() * a.get_z() - get_z() * a.get_y()), (get_z * a.get_x - get_x * a.get_z), (get_x * a.get_y - get_y * a.get_x)); }
         //2X2 Matrix
-        T constexpr magnitude();
-        Vector<T> &normalize();
+        T magnitude() const
+        {
+            return sqrt(x_pos * x_pos + y_pos * y_pos + z_pos * z_pos);
+        }
+        Vector<T> &normalize()
+        {
+            T inverse = 1 / magnitude();
+            x_pos *= inverse;
+            y_pos *= inverse;
+            z_pos *= inverse;
+            return *this;
+        }
+        inline Vector<T> unit_vector()
+        {
+            T d = this->magnitude();
+            return *this / d;
+        }
 
     private:
-        T x_pos,
+           T x_pos,
             y_pos,
             z_pos;
     };
 } // namespace raycast
 
-//Typedef
+//Typedefs
 typedef raycast::Vector<float> vecfloat;
 typedef raycast::Vector<int> vecint;
 typedef raycast::Vector<double> vecdouble;
