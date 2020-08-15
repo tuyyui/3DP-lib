399d4bc17583a465f25b02687abafa5032a06b40
tuyyui <mrjustinyoung991@gmail.com>
2020-08-14 20:56:30 -0500

    Useless file
    This file is useless but you can initialize the template functions in the cc file if you wish.

--- src/raycastengine.cc
+++ src/raycastengine.cc
@@ -1,30 +1,3 @@
 #include "include/raycastengine.h"
 
-template <typename T>
-T raycast::Vector<T>::dot_product(Vector<T> a)
-{
-    return get_x() * a.get_x() + get_y * a.get_y + get_z * a.get_z;
-}
-
-template <typename T>
-raycast::Vector<T> raycast::Vector<T>::cross_product(Vector<T> a)
-{
-    return Vector<T>((get_y() * a.get_z() - get_z() * a.get_y()), (get_z * a.get_x - get_x * a.get_z), (get_x * a.get_y - get_y * a.get_x));
-}
-
-template <typename T>
-T constexpr raycast::Vector<T>::magnitude()
-{
-    return sqrt(get_x * get_x + get_y * get_y + get_z * get_z);
-}
-
-template <typename T>
-
-raycast::Vector<T> &raycast::Vector<T>::normalize()
-{
-    T inverse = 1 / magnitude();
-    x_pos *= inverse;
-    y_pos *= inverse;
-    z_pos *= inverse;
-    return *this;
-}
+//You can add declarations to this file if you wish.
