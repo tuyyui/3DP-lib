6dac4977a6cba275078729d0f4654316b347551e
tuyyui <mrjustinyoung991@gmail.com>
2020-08-24 23:59:07 -0500

    Update sphere.h

--- src/include/sphere.h
+++ src/include/sphere.h
@@ -6,13 +6,44 @@
 class sphere : public hittable
 {
 public:
-    //sphere(){};
+    sphere() {}
+    ~sphere();
     sphere(vecfloat cen, float r) : center(cen), radius(r) {}
-    virtual bool hit(const ray &r, float t_min, float t_max, hit_record &rec);
+    bool hit(const ray &r, float t_min, float t_max, hit_record &rec) const
+    {
+        vecfloat oc = r.origin() - center;
+        vecfloat getr = r.direction();
+        float a = getr.dot_product(r.direction());
+        float b = oc.dot_product(r.direction());
+        float c = oc.dot_product(oc) - (radius * radius);
+        float discriminant = b * b - a * c;
+        if (discriminant > 0)
+        {
+            float temp = (-b - sqrt(b * b - a * c)) / a;
+            if (temp < t_max && temp > t_min)
+            {
+                rec.t = temp;
+                rec.p = r.point_at_parameter(rec.t);
+                rec.normal = (rec.p - center) / radius;
+                return true;
+            }
+            temp = (-b + sqrt(b * b - a * c)) / a;
+            if (temp < t_max && temp > t_min)
+            {
+                rec.t = temp;
+                rec.p = r.point_at_parameter(rec.t);
+                rec.normal = (rec.p - center) / radius;
+                return true;
+            }
+        }
+        return false;
+    }
 
 protected:
     vecfloat center;
     float radius;
 };
-
+sphere::~sphere()
+{
+}
 #endif
